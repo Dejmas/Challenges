899a7c059571d4c19bbea081716062e341acf1f3
Tomáš Dejmek <dejmektomas@gmail.com>
2024-09-02 13:50:18 +0200

    rename cost flow network

--- Greedy Island/min-cost-flow.cpp
+++ Greedy Island/min-cost-flow.cpp
@@ -3,45 +3,47 @@
 #include <iostream>
 #include <set>
 
-class NetworkCostFlow {
+// Proper implementation
+class CostFlowNetwork {
     public: 
-        NetworkCostFlow(size_t noNodes);
+        CostFlowNetwork(size_t noNodes);
         void addEdge(int a, int b, long cap, long cost);
         long minCostFlow(int source, int target, long flowLimit);
 
-        static long INF; 
+        struct Edge {
+            int from, to;
+            long capacity, flow = 0, cost;
+            Edge (int a, int b, long capacity, long cost)
+            : from(a), to(b), capacity(capacity), cost(cost) {
+            }
+
+            long residue() const {
+                return capacity - flow;
+            }
+        };
+
+        inline static long INF = 1e18;
 
     private:
         void augmentingPath(int source, int target, std::vector<int> & parents, std::vector<long> & costs) const;
 
         std::vector<std::vector<int>> m_adjacent;
-        std::vector<std::vector<long>> m_residues;
-        std::vector<std::vector<long>> m_cost;
-        std::set<std::pair<int, int>> m_existingEdges;
+        std::vector<Edge> m_edges;
 };
 
-long NetworkCostFlow::INF = 1e18;
-
-NetworkCostFlow::NetworkCostFlow(size_t noNodes) 
-: m_adjacent(noNodes)
-, m_residues(noNodes, std::vector<long>(noNodes, 0))
-, m_cost(noNodes, std::vector<long>(noNodes, 0)) {
+CostFlowNetwork::CostFlowNetwork(size_t noNodes) 
+: m_adjacent(noNodes){
 }
 
-void NetworkCostFlow::addEdge(int a, int b, long cap, long cost) {
-    if (m_existingEdges.count({a,b}) == 0 && m_existingEdges.count({b,a}) == 0) {
-        m_adjacent[a].push_back(b);
-        m_adjacent[b].push_back(a);
-        m_residues[a][b] = cap;
-        m_cost[a][b] = cost;
-        m_cost[b][a] += -cost;
-        m_existingEdges.insert({a,b});
-    } else {
-        throw std::invalid_argument("either parallel edge or counter edge to some other.");
-    }
+void CostFlowNetwork::addEdge(int a, int b, long cap, long cost) {
+    auto m = m_edges.size();
+    m_edges.emplace_back(Edge{a, b, cap, cost});
+    m_adjacent[a].push_back(m ++);
+    m_edges.emplace_back(Edge{b, a, 0, -cost});
+    m_adjacent[b].push_back(m);
 }
 
-long NetworkCostFlow::minCostFlow(int source, int target, long flowLimit) {
+long CostFlowNetwork::minCostFlow(int source, int target, long flowLimit) {
     long flow = 0;
     long cost = 0;
 
@@ -55,31 +57,31 @@ long NetworkCostFlow::minCostFlow(int source, int target, long flowLimit) {
         // find max flow
         long path_flow = flowLimit - flow;
         for (int current = target; current != source; ) {
-            int prev = parents[current];
-            path_flow = std::min(path_flow, m_residues[prev][current]);
-            current = prev;
+            Edge edge = m_edges[parents[current]];
+            path_flow = std::min(path_flow, edge.residue());
+            current = edge.from;
         }
- 
+
         // apply flow
         flow += path_flow;
         cost += path_flow * costs[target];
-        // std::cout << "augmenting path " << path_flow << ": ";
+        // std::cerr << "augmenting path " << path_flow << ": ";
         for (int current = target; current != source; ) {
-            int prev = parents[current];
-            m_residues[prev][current] -= path_flow;
-            m_residues[current][prev] += path_flow;
-            // std::cout << current << " -> " << prev << " ";
-            current = prev;
+            int idx = parents[current];
+            m_edges[idx].flow += path_flow;
+            m_edges[idx ^ 1].flow -= path_flow;
+            // std::cerr << current << " -> " << m_edges[idx].from << " ";
+            current = m_edges[idx].from;
         }
-        // std::cout << "\n";
+        // std::cerr << "\n";
     }
 
+    // std::cerr << "flow " << flow << " cost " << cost << "\n";
     if (flow < flowLimit) return -1;
-
     return cost;
 }
 
-void NetworkCostFlow::augmentingPath(int source, int target, std::vector<int> & parents, std::vector<long> & costs) const{
+void CostFlowNetwork::augmentingPath(int source, int target, std::vector<int> & parents, std::vector<long> & costs) const{
     size_t N = m_adjacent.size();
     costs.assign(N, INF);
     parents.assign(N, -1);
@@ -91,13 +93,14 @@ void NetworkCostFlow::augmentingPath(int source, int target, std::vector<int> & 
         q.pop_front();
         inQ[current] = false;
 
-        for (int next : m_adjacent[current]) {
-            if (m_residues[current][next] > 0 && costs[next] > costs[current] + m_cost[current][next]) {
-                costs[next] = costs[current] + m_cost[current][next];
-                parents[next] = current;
-                if (!inQ[next]) {
-                    inQ[next] = true;
-                    q.push_back(next);
+        for (int idx : m_adjacent[current]) {
+            auto edge = m_edges[idx];
+            if (edge.residue() > 0 && costs[edge.to] > costs[current] + edge.cost) {
+                costs[edge.to] = costs[current] + edge.cost;
+                parents[edge.to] = idx;
+                if (!inQ[edge.to]) {
+                    inQ[edge.to] = true;
+                    q.push_back(edge.to);
                 }
             }
         }
@@ -122,7 +125,7 @@ bool GREED_Greedy_island () {
     const int source = 0;
     const int target = N+1;
 
-    NetworkCostFlow network(N+2);
+    CostFlowNetwork network(N+2);
 
     for (int i = 1; i <= N; i ++) {
         network.addEdge(source, i, cards[i], 0);
@@ -139,9 +142,9 @@ bool GREED_Greedy_island () {
         if (!(std::cin >> from >> to)) {
             return false;
         }
-        network.addEdge(from, to, NetworkCostFlow::INF, 1);
+        network.addEdge(from, to, CostFlowNetwork::INF, 1);
         // unfortunately
-        // network.addEdge(to, from, NetworkCostFlow::INF, 1);
+        network.addEdge(to, from, CostFlowNetwork::INF, 1);
     }
 
     auto flow = network.minCostFlow(source, target, N);
